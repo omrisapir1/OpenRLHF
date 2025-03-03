d22557e533dffc3e21482d14f862ddfe3aee3de0
Omri <omri.sapir@workday.com>
2025-03-03 12:56:16 +0200

    all

---
+++ examples/scripts/reward_func_2.py
@@ -0,0 +1,32 @@
+import torch
+import re
+import json
+import os
+
+PATH = '/kaggle/working/OpenRLHF/batch_solution'
+def reward_func(queries, prompts, labels):
+    if os.path.exists(PATH):
+        cur_i = max([b.split('.json')[0] for b in os.listdir(PATH) if b.endswith('.json')])
+        cur_i = int(cur_i) + 1
+        json.dump(list(zip(queries, labels)),open(f'{PATH}/{cur_i}.json','w'))
+    # queries is prompts + responses
+    # labels is answers
+    rewards = [reward_math_func(q, l) for q, l in zip(queries, labels)]
+    return torch.tensor(rewards, dtype=torch.float)
+
+
+def extract_numerical_answer(answer):
+    """Extract the numerical answer enclosed in \boxed{}"""
+    match = re.search(r'\\boxed\{([^}]*)\}', answer)
+    if match:
+        extracted_answer = match.group(1)
+        extracted_answer = re.sub(r'[^0-9.]', '', extracted_answer).strip()
+        if extracted_answer.replace('.', '', 1).isdigit():
+            return extracted_answer
+    return None
+
+def reward_math_func(answer, numerical_solution):
+    extracted_answer = extract_numerical_answer(answer)
+    if extracted_answer is None:
+        return 0
+    return int(float(numerical_solution) == float(extracted_answer))
