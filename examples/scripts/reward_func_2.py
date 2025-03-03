aee0da5032240ea3f9050da26d3a5a30ac75fb97
Omri <omri.sapir@workday.com>
2025-03-03 15:10:54 +0200

    all

--- examples/scripts/reward_func_2.py
+++ examples/scripts/reward_func_2.py
@@ -8,14 +8,14 @@ PATH = '/kaggle/working/OpenRLHF/batch_solution'
 def reward_func(queries, prompts, labels):
     if os.path.exists(PATH):
         try:
-            cur_i = max([b.split('.json')[0] for b in os.listdir(PATH) if b.endswith('.json')])
+            cur_i = max([b.split('.json')[0].split('_')[0] for b in os.listdir(PATH) if b.endswith('.json')])
             cur_i = int(cur_i) + 1
         except:
             cur_i = 0
     else:
         os.mkdir(PATH)
         cur_i = 0
-    json.dump(list(zip(queries, labels.cpu().tolist()),open(f'{PATH}/{cur_i}_{time.time()}.json','w')))
+    json.dump(list(zip(queries, labels),open(f'{PATH}/{cur_i}_{time.time()}.json','w')))
     # queries is prompts + responses
     # labels is answers
     rewards = [reward_math_func(q, l) for q, l in zip(queries, labels)]
