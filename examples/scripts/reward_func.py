156e7c4856243f3ec0b83c498b24f7441631c8f5
Omri <omri.sapir@workday.com>
2025-03-04 19:18:07 +0200

    all

--- examples/scripts/reward_func.py
+++ examples/scripts/reward_func.py
@@ -2,13 +2,21 @@ import torch
 import re
 import json
 import os
+import time
 
 PATH = '/kaggle/working/OpenRLHF/batch_solution'
 def reward_func(queries, prompts, labels):
     if os.path.exists(PATH):
-        cur_i = max([b.split('.json')[0] for b in os.listdir(PATH) if b.endswith('.json')])
-        cur_i = int(cur_i) + 1
-        json.dump(list(zip(queries, labels)),open(f'{PATH}/{cur_i}.json','w'))
+        try:
+            cur_i = max([int(b.split('.json')[0].split('_')[0]) for b in os.listdir(PATH) if b.endswith('.json')])
+            cur_i = int(cur_i) + 1
+        except:
+            cur_i = 0
+    else:
+        os.mkdir(PATH)
+        cur_i = 0
+
+    json.dump(list(zip(queries, [t.item() for t in labels])),open(f'{PATH}/{cur_i}_{time.time()}.json','w'))
     # queries is prompts + responses
     # labels is answers
     rewards = [reward_math_func(q, l) for q, l in zip(queries, labels)]
@@ -29,6 +37,4 @@ def reward_math_func(answer, numerical_solution):
     extracted_answer = extract_numerical_answer(answer)
     if not extracted_answer:
         return 0
-    print(extracted_answer)
-    print("Numerical: " + numerical_solution)
     return int(float(numerical_solution) == float(extracted_answer))
